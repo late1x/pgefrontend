9ff0843088be2772ecb73c0de6772a9ebf77e210
Luis Alejandro Torres Escalera <88306161+late1x@users.noreply.github.com>
2023-05-24 11:15:52 -0600

    update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
-  server: {
-    port: 8080,
-  },
-});
+})
